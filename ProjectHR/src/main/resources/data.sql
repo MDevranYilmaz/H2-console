f45fb16cc25e9cfd296199d1a1e211ee36472f31
Devran Yılmaz <mdevranyilmaz1@gmail.com>
2025-07-22 21:21:23 +0300

    Configures approval service and updates ports

--- ProjectHR/src/main/resources/data.sql
+++ ProjectHR/src/main/resources/data.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+DELETE FROM users;
 INSERT INTO users (id, first_name, last_name, role, email, username, password)
 VALUES
     ( gen_random_uuid(), 'HR User 1', 'Lastname 1', 'HR', 'hr1@example.com', 'hruser1', 'password1'),
