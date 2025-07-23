ff30954aaa0e35ab4b82bac496493ca1cfe0f351
Devran Yılmaz <mdevranyilmaz1@gmail.com>
2025-07-23 14:27:18 +0300

    integration of .env file

--- ProjectHR/src/main/java/com/ProjectHR/service/UserService.java
+++ ProjectHR/src/main/java/com/ProjectHR/service/UserService.java
@@ -1,7 +1,6 @@
 package com.ProjectHR.service;
 
 import com.ProjectHR.grpc.ApprovalServiceGRPCClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
